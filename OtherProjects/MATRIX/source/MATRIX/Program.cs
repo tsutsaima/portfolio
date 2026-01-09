3319b4e7ba52187cdbcc13d0679a8be4770d144f
tsutsaima <125797003+tsutsaima@users.noreply.github.com>
2026-01-09 19:47:31 +0100

    Add files via upload

---
+++ OtherProjects/MATRIX/source/MATRIX/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATRIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.ForegroundColor = ConsoleColor.Green;
+            do
+            {
+                for (int p = 160; p > 0; p = p - 1)
+                {
+                    int pes = rnd.Next(0, 9);
+                    //Console.ForegroundColor = (ConsoleColor) pes;
+                    Console.Write(pes);
+                    
+                }
+                
+            } while (true);
+
+
+
+            
+        }
+    }
+}
